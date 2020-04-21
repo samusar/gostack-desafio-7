30852f0db2992bb0ca5307931c65691955c1af65
Samuel Henrique Santos Monção <samu.henr@gmail.com>
2020-04-21 17:49:06 -0300

    resolução do desafio

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,30 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.map((file) => {
+      data.set('file', file.file);
+    });
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
+    history.goBack();
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    setUploadedFiles(
+      files.map((file: File) => {
+        return {
+          file,
+          name: file.name,
+          readableSize: filesize(file.size),
+        };
+      }),
+    );
   }
 
   return (
